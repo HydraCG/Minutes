7990310589e3b545d13c129830441e44e07911b0
Markus Lanthaler <mark_lanthaler@gmx.net>
2017-06-12 21:09:02 +0200

    Fix typo in Karol's nickname

--- scribetool/scrawl-config.js
+++ scribetool/scrawl-config.js
@@ -21,7 +21,7 @@
     "Thomas Bergwinkl": { "alias": ["bergi"] },
     "elf Pavlik": { "alias": ["elf-pavlik"] },
     "Tomasz Pluskiewicz": { "alias": ["tpluskiewicz"] },
-    "Karol Szczepański": { "alias": ["karol_szzczepanski", "karol-szczepanski"] },
+    "Karol Szczepański": { "alias": ["karol_szczepanski", "karol-szczepanski"] },
   };
   scrawl.htmlHeader = `<!doctype html>
 <html lang="en">
